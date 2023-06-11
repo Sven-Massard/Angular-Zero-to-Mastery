d41ce8cac660e26c65ca7431e43d22b31b46e20d
Sven Massard <svemass@gmail.com>
2023-06-11 22:33:51 +0200

    Basics, Chapter 3 Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
+  // template: `<p>Hello World!</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css'],
+  // styles: ['']
 })
-export class AppComponent {
-  title = 'basics';
+export class AppComponent{
+
 }
